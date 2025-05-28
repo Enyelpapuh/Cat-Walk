d5ed18788d123246e6513b9b49f35c29102854c7
Aura <auraperezfl3@gmail.com>
2025-05-28 14:48:56 -0600

    implementacion de Skybox, dando iluminacion al entorno

--- CatProyect/Cfiles/Main.cpp
+++ CatProyect/Cfiles/Main.cpp
@@ -88,6 +88,43 @@ void renderImGui()
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
+//skyboxes data
+float skyboxVertices[] =
+{
+	//   Coordinates
+	-1.0f, -1.0f,  1.0f,//        7--------6
+	 1.0f, -1.0f,  1.0f,//       /|       /|
+	 1.0f, -1.0f, -1.0f,//      4--------5 |
+	-1.0f, -1.0f, -1.0f,//      | |      | |
+	-1.0f,  1.0f,  1.0f,//      | 3------|-2
+	 1.0f,  1.0f,  1.0f,//      |/       |/
+	 1.0f,  1.0f, -1.0f,//      0--------1
+	-1.0f,  1.0f, -1.0f
+};
+
+unsigned int skyboxIndices[] =
+{
+	// Right
+	1, 2, 6,
+	6, 5, 1,
+	// Left
+	0, 4, 7,
+	7, 3, 0,
+	// Top
+	4, 5, 6,
+	6, 7, 4,
+	// Bottom
+	0, 3, 2,
+	2, 1, 0,
+	// Back
+	0, 1, 5,
+	5, 4, 0,
+	// Front
+	3, 7, 6,
+	6, 2, 3
+};
+
+
 int main()
 {
 	// Sonidos 
@@ -102,7 +139,82 @@ int main()
 
 
 	GLFWwindow* window = initOpenGL();
-	Shader ourShader("anim_model.vs", "anim_model.fs");
+	Shader ourShader("Assets/Shaders/anim_model.vs", "Assets/Shaders/anim_model.fs");
+	Shader ourShader2("skybox.vert", "skybox.frag");
+	ourShader.use();
+	ourShader.setVec3("ambientLightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+	ourShader.setFloat("ambientStrength", 0.9f);
+	ourShader2.use();
+	ourShader2.setFloat("skyboxIntensity", 0.5f);
+
+
+	// Create VAO, VBO, and EBO for the skybox
+	unsigned int skyboxVAO, skyboxVBO, skyboxEBO;
+	glGenVertexArrays(1, &skyboxVAO);
+	glGenBuffers(1, &skyboxVBO);
+	glGenBuffers(1, &skyboxEBO);
+	glBindVertexArray(skyboxVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, skyboxEBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(skyboxIndices), &skyboxIndices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// All the faces of the cubemap (make sure they are in this exact order)
+	std::string facesCubemap[6] =
+	{
+		"Assets/skybox/right.jpg",
+		"Assets/skybox/left.jpg",
+		"Assets/skybox/top.jpg",
+		"Assets/skybox/bottom.jpg",
+		"Assets/skybox/front.jpg",
+		"Assets/skybox/back.jpg"
+	};
+	// Creates the cubemap texture object
+	unsigned int cubemapTexture;
+	glGenTextures(1, &cubemapTexture);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	// These are very important to prevent seams
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+	// This might help with seams on some systems
+	//glEnable(GL_TEXTURE_CUBE_MAP_SEAMLESS);
+
+	// Cycles through all the textures and attaches them to the cubemap object
+	for (unsigned int i = 0; i < 6; i++)
+	{
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load(facesCubemap[i].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			stbi_set_flip_vertically_on_load(false);
+			glTexImage2D
+			(
+				GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+				0,
+				GL_RGB,
+				width,
+				height,
+				0,
+				GL_RGB,
+				GL_UNSIGNED_BYTE,
+				data
+			);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Failed to load texture: " << facesCubemap[i] << std::endl;
+			stbi_image_free(data);
+		}
+	}
 
 
 	// Configuracion donde se carga el modelo y las animaciones a ocupar de ese modelo
@@ -257,18 +369,35 @@ int main()
 		{
 			
 			processInput(window);
+
 			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+			// -------------------- ACTUALIZAR ANIMACIONES --------------------
 			catAnimator1.UpdateAnimation(deltaTime);
 
-
-			float currentTime = glfwGetTime();
-
-			ourShader.use();
-
+			// -------------------- SKYBOX --------------------
 			glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 			glm::mat4 view = camera.GetViewMatrix();
+
+			glDepthFunc(GL_LEQUAL);  // Permitir z igual
+			glDepthMask(GL_FALSE);   // Desactivar escritura de profundidad
+
+			ourShader2.use();
+			ourShader2.setMat4("view", glm::mat4(glm::mat3(view))); // sin traslación
+			ourShader2.setMat4("projection", projection);
+
+			glBindVertexArray(skyboxVAO);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+			glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			glDepthMask(GL_TRUE);    // Restaurar escritura
+			glDepthFunc(GL_LESS);    // Restaurar profundidad estándar
+
+			// -------------------- MODELO ANIMADO --------------------
+			ourShader.use();
 			ourShader.setMat4("projection", projection);
 			ourShader.setMat4("view", view);
 
@@ -281,6 +410,7 @@ int main()
 			model1 = glm::scale(model1, glm::vec3(0.01f));
 			ourShader.setMat4("model", model1);
 			catM1.Draw(ourShader);
+
 
 
 			glm::vec3 camPos = camera.Position;
